bf3f2500012e2a6e6126dfced16c6760b1fa0823
IsaqueMansur <isaquemansur@gmail.com>
2023-05-30 16:13:50 -0300

    try push infos on software in PM2 at Release

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@
     {
       name: "WEB_API_ASP",
       script: "dotnet",
-          args: "run --project ./WEB_API_ASP.csproj",
+          args: "run --project .\bin\Release\net7.0\WEB_API_ASP.dll",
       watch: true,
       ignore_watch: ["node_modules", "client"],
       env: {
