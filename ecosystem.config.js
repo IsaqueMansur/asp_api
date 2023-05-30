2142d41acf469dc116185564127019ddca38d957
IsaqueMansur <isaquemansur@gmail.com>
2023-05-30 16:04:30 -0300

    add config to PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+  apps: [
+    {
+      name: "WEB_API_ASP",
+      script: "dotnet",
+          args: "run --project ./WEB_API_ASP.csproj",
+      watch: true,
+      ignore_watch: ["node_modules", "client"],
+      env: {
+        ASPNETCORE_ENVIRONMENT: "Production",
+      },
+    },
+  ],
+};
